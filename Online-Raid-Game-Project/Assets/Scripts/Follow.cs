7df4283d7b8560b39751c9bfb36f3b36ca8f0442
PatFagan <patrickfagan1586@gmail.com>
2021-10-11 10:07:32 -0400

    Fixed Grappling Hook Online
    + except grappling hooks pull other players

--- Online-Raid-Game-Project/Assets/Scripts/Follow.cs
+++ Online-Raid-Game-Project/Assets/Scripts/Follow.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class Follow : Photon.MonoBehaviour
 {
     public float speed;
     private Transform target;
     public string targetTag;
+    public PhotonView photonView;
 
     void FixedUpdate()
     {
-        if (GameObject.FindGameObjectWithTag(targetTag))
+        if (photonView.isMine)
         {
-            // get target
-            target = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Transform>();
-            // move towards target
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            if (GameObject.FindGameObjectWithTag(targetTag))
+            {
+                // get target
+                target = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Transform>();
+                // move towards target
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            }
         }
     }
 }
--- Online-Raid-Game-Project/Assets/Scripts/GrapplingHook.cs
+++ Online-Raid-Game-Project/Assets/Scripts/GrapplingHook.cs
@@ -7,13 +7,17 @@ public class GrapplingHook : MonoBehaviour
     public float grapplingSpeed;
     Rigidbody2D rigidbody;
     bool grapplingMovement;
-
+    GameObject player;
     Player playerScript;
+    PhotonView photonView;
+
     void Start()
     {
         grapplingMovement = false;
         // get player script
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        player = GameObject.Find("Player1");
+        photonView = player.GetComponent<PhotonView>();
+        playerScript = player.GetComponent<Player>();
         rigidbody = GetComponent<Rigidbody2D>(); // get rigidbody
     }
 
@@ -29,7 +33,7 @@ public class GrapplingHook : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector2 playerPos = player.transform.position;
         float dist = Mathf.Sqrt(Mathf.Pow(playerPos.x - transform.position.x, 2)
             + Mathf.Pow(playerPos.y - transform.position.y, 2));
         //Debug.Log(dist);
@@ -39,13 +43,16 @@ public class GrapplingHook : MonoBehaviour
         }
         if (grapplingMovement)
         {
-            // movetowards(this_pos, target, step)
-            GameObject.FindGameObjectWithTag("Player").transform.position =
-                Vector2.MoveTowards(
-                    GameObject.FindGameObjectWithTag("Player").transform.position,
-                    transform.position,
-                    grapplingSpeed * Time.fixedDeltaTime
-                );
+            if (photonView.isMine)
+            {
+                // movetowards(this_pos, target, step)
+                player.transform.position =
+                    Vector2.MoveTowards(
+                        playerPos,
+                        transform.position,
+                        grapplingSpeed * Time.fixedDeltaTime
+                    );
+            }
         }
     }
 }
