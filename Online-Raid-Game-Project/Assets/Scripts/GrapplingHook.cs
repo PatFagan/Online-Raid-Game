8336074a871e0431407c98ea449ac089e2f427fa
PatFagan <patrickfagan1586@gmail.com>
2021-10-10 20:15:12 -0400

    Attempted Online Fixes for Grappling and Aimed Projectiles

--- Online-Raid-Game-Project/Assets/Scripts/GrapplingHook.cs
+++ Online-Raid-Game-Project/Assets/Scripts/GrapplingHook.cs
@@ -7,45 +7,55 @@ public class GrapplingHook : MonoBehaviour
     public float grapplingSpeed;
     Rigidbody2D rigidbody;
     bool grapplingMovement;
+    public PhotonView photonView;
 
     Player playerScript;
     void Start()
     {
-        grapplingMovement = false;
-        // get player script
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        rigidbody = GetComponent<Rigidbody2D>(); // get rigidbody
+        if (photonView.isMine)
+        {
+            grapplingMovement = false;
+            // get player script
+            playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            rigidbody = GetComponent<Rigidbody2D>(); // get rigidbody
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        // if collides with wall
-        if (collider.gameObject.tag == "Wall")
+        if (photonView.isMine)
         {
-            rigidbody.velocity = new Vector2(0,0); // stop hook movement
-            grapplingMovement = true;
+            // if collides with wall
+            if (collider.gameObject.tag == "Wall")
+            {
+                rigidbody.velocity = new Vector2(0, 0); // stop hook movement
+                grapplingMovement = true;
+            }
         }
     }
 
     void FixedUpdate()
     {
-        Vector2 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        float dist = Mathf.Sqrt(Mathf.Pow(playerPos.x - transform.position.x, 2)
-            + Mathf.Pow(playerPos.y - transform.position.y, 2));
-        Debug.Log(dist);
-        if (dist < 3)
+        if (photonView.isMine)
         {
-            grapplingMovement = false;
-        }
-        if (grapplingMovement)
-        {
-            // movetowards(this_pos, target, step)
-            GameObject.FindGameObjectWithTag("Player").transform.position =
-                Vector2.MoveTowards(
-                    GameObject.FindGameObjectWithTag("Player").transform.position,
-                    transform.position,
-                    grapplingSpeed * Time.fixedDeltaTime
-                );
+            Vector2 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+            float dist = Mathf.Sqrt(Mathf.Pow(playerPos.x - transform.position.x, 2)
+                + Mathf.Pow(playerPos.y - transform.position.y, 2));
+            Debug.Log(dist);
+            if (dist < 3)
+            {
+                grapplingMovement = false;
+            }
+            if (grapplingMovement)
+            {
+                // movetowards(this_pos, target, step)
+                GameObject.FindGameObjectWithTag("Player").transform.position =
+                    Vector2.MoveTowards(
+                        GameObject.FindGameObjectWithTag("Player").transform.position,
+                        transform.position,
+                        grapplingSpeed * Time.fixedDeltaTime
+                    );
+            }
         }
     }
 }
--- Online-Raid-Game-Project/Assets/Scripts/Projectiles/AimedProjectile.cs
+++ Online-Raid-Game-Project/Assets/Scripts/Projectiles/AimedProjectile.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AimedProjectile : MonoBehaviour
+public class AimedProjectile : Photon.MonoBehaviour
 {
     Rigidbody2D rigidbody;
     public GameObject deathEffect;
     public float speed;
 
     Player playerScript;
+    public PhotonView photonView;
 
     void Start()
     {
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        if (photonView.isMine)
+        {
+            playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 
-        // set direction/speed
-        rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.velocity = playerScript.movement * speed;
+            // set direction/speed
+            rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody.velocity = playerScript.movement * speed;
+        }
     }
 }
