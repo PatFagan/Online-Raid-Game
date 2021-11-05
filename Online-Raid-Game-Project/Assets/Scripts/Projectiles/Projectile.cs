918a5777f3024c239119c2c8dceb450523d24594
PatFagan <patrickfagan1586@gmail.com>
2021-11-05 08:35:49 -0400

    Boss Fight
    + added to boss fight

--- Online-Raid-Game-Project/Assets/Scripts/Projectiles/Projectile.cs
+++ Online-Raid-Game-Project/Assets/Scripts/Projectiles/Projectile.cs
@@ -7,21 +7,23 @@ public class Projectile : Photon.MonoBehaviour
     public float projectileLifespan;
     public GameObject deathEffect;
     const int NUM_OF_TAGS = 5; 
-    public string[] targetTag = new string[NUM_OF_TAGS];
+    public string[] destroyTag = new string[NUM_OF_TAGS];
     public int damage;
     int i;
+    public bool lifespan = true;
 
     void Start()
     {
-        Destroy(gameObject, projectileLifespan); // destroy after lifespan expires
+        if (lifespan)
+            Destroy(gameObject, projectileLifespan); // destroy after lifespan expires
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         // if collides with target, destroy
-        for (i = 0; i < targetTag.Length; i++)
+        for (i = 0; i < destroyTag.Length; i++)
         {
-            if (collider.gameObject.tag == targetTag[i])
+            if (collider.gameObject.tag == destroyTag[i])
             {
                 if (deathEffect) { Instantiate(deathEffect, transform.position, Quaternion.identity); }
                 Destroy(gameObject);
